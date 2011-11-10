2824201b2f6368d83caf9c262cbb4d4de8d85114
Charles Moulliard <cmoulliard@gmail.com>
2011-11-10 14:39:20 +0100

    Clean routing module

--- routing/src/main/java/org/fusesource/devoxx/reportincident/internal/Feedback.java
+++ routing/src/main/java/org/fusesource/devoxx/reportincident/internal/Feedback.java
@@ -16,14 +16,7 @@
  */
 package org.fusesource.devoxx.reportincident.internal;
 
-import org.fusesource.devoxx.reportincident.OutputReportIncident;
-
 public class Feedback {
 
-    public OutputReportIncident setOk() {
-        OutputReportIncident outputReportIncident = new OutputReportIncident();
-        outputReportIncident.setCode("0");
-        return outputReportIncident;
-    }
 
 }
--- routing/src/main/java/org/fusesource/devoxx/reportincident/internal/IncidentSaver.java
+++ routing/src/main/java/org/fusesource/devoxx/reportincident/internal/IncidentSaver.java
@@ -16,78 +16,7 @@
  */
 package org.fusesource.devoxx.reportincident.internal;
 
-import org.apache.camel.Exchange;
-import org.fusesource.devoxx.reportincident.model.Incident;
-import org.fusesource.devoxx.reportincident.service.IncidentService;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-
 public class IncidentSaver {
 
-    private static final transient Log LOG = LogFactory.getLog(IncidentSaver.class);
-    
-    private IncidentService incidentService = null;
-
-    public void process(Exchange exchange) throws ParseException {
-
-        int count = 0;
-
-        List<Map<String, Object>> models = new ArrayList<Map<String, Object>>();
-        Map<String, Object> model = new HashMap<String, Object>();
-
-        // Get models from message
-        models = (List<Map<String, Object>>) exchange.getIn().getBody();
-        
-        // Get Header origin from message
-        String origin = (String) exchange.getIn().getHeader("origin");
-        LOG.debug("Header origin : " + origin);
-
-        Iterator<Map<String, Object>> it = models.iterator();
-        
-        // Specify current Date
-        DateFormat format = new SimpleDateFormat( "dd/MM/yyyy HH:mm:ss" );
-        String currentDate = format.format( new Date() );
-        Date creationDate = format.parse( currentDate );
-        
-        while (it.hasNext()) {
-
-            model = it.next();
-            
-            LOG.debug("Model retrieved");
-
-            for (String key : model.keySet()) {
-                
-                LOG.debug("Object retrieved : " + model.get(key).toString());
-                
-                // Retrieve incident from model
-                Incident incident = (Incident) model.get(key);
-                incident.setCreationDate(creationDate);
-                incident.setCreationUser(origin);
-                
-                LOG.debug("Count : " + count + ", " + incident.toString());
-                
-                // Save org.fusesource.devoxx.reportincident.model.Incident
-                incidentService.saveIncident(incident);
-                LOG.debug(">>> org.fusesource.devoxx.reportincident.model.Incident saved");
-            }
-
-            count++;
-        }
-
-        LOG.debug("Nber of CSV records received by the csv bean : " + count);
-
-    }
-    
-    // Property used to inject service implementation
-    public void setIncidentService(IncidentService incidentService) {
-        this.incidentService = incidentService;
-    }
-    
-    
 
 }
